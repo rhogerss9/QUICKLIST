bc04132e01ff8d9c9011fe4aeee84d8daa50354c
rhoger souza <rhoger.silva@eaportal.org>
2025-06-01 14:05:17 -0300

    criação da função showitemslist para renderizar na tela os itens do array

--- assets-20250601T022731Z-1-001/assets/main.js
+++ assets-20250601T022731Z-1-001/assets/main.js
@@ -1,7 +1,12 @@
-const items = []
+let items = []
 
 function addItem(){
     const itemName = document.querySelector("#item").value
+
+    if(itemName === ""){
+        alert("Digite um Item Válido")
+        return
+    }
 
     const item = {
         name: itemName,
@@ -9,6 +14,34 @@ function addItem(){
     }
 
     items.push(item)
-
+    
     document.querySelector("#item").value = ""
+
+  showItemsList()
+}
+
+function showItemsList(){
+    const sectionList = document.querySelector(".list") 
+    sectionList.innerHTML = ""
+
+    items.sort((itemA, itemB)=> Number(itemA.checked) - Number(itemB.checked))
+    
+
+    items.map((item, index) => {
+        sectionList.innerHTML +=`
+         <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}" ${item.checked === true ? "checked" : ""}>
+                <div class="custom-checkbox" onclick="checkItem('${item.name}')">
+                    <img src="checked.svg" alt="checked">
+                </div>
+                <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
+
+                </div>
+                <button onclick = "removeItem('${item.name}')">
+                    <img src="trash-icon.svg" alt="trash icon">
+                </button>
+            </div>
+        `
+    })
 }
